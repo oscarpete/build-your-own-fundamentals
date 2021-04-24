1ab42165c212ce556bde079e3000389b05b14c92
Panda Guy <59342211+oscarpete@users.noreply.github.com>
2021-04-24 08:40:09 +0200

    update

---
+++ Views/home.php
@@ -0,0 +1,12 @@
+<?php require 'templates/header.php'; ?>
+
+<div class="homepage">
+
+    <h2>What you want to see: Articles or Products?</h2>
+</div>
+
+<?php require 'templates/footer.php'; ?>
+
+
+
+
